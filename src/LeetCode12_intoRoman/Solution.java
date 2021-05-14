8d22fdabd78ac7b7da00fbe8fef7f2462b46820a
skyliuhc <1684543761@qq.com>
2021-05-14 10:40:51 +0800

    新增一道每日一题的解法from idea

--- src/LeetCode12_intoRoman/Solution.java
+++ src/LeetCode12_intoRoman/Solution.java
@@ -1,9 +1,15 @@
 package LeetCode12_intoRoman;
 
 public class Solution {
+    //方法一：模拟
+//    根据罗马数字的唯一表示法，为了表示一个给定的整数 num，
+//    我们寻找不超过num 的最大符号值，将 num 减去该符号值，
+//    然后继续寻找不超过 num 的最大符号值，将该符号拼接在上一个找到的符号之后，
+//    循环直至num 为 00。最后得到的字符串即为num 的罗马数字表示
+
     int[] values ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
     String[] symbols ={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    public String inToRoman(int num){
+    public String intToRoman(int num){
         StringBuffer roman = new StringBuffer();
         for (int i = 0; i < values.length; i++) {
             int value = values[i];
@@ -21,7 +27,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.inToRoman(3));
+        System.out.println(s.intToRoman(3));
     }
 
 
